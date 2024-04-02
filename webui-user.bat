17760831cb2f38f71d04931e799baac3434dc3d9
qiaoyafeng <qiaoyafeng@gmail.com>
2024-04-02 15:12:53 +0800

    update windows args

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,7 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--port 32105 --listen --xformers  --skip-torch-cuda-test --precision full --no-half --no-gradio-queue --log-startup --loglevel DEBUG --api --api-log  --device-id 0 --medvram --enable-insecure-extension-access
+
 
 call webui.bat
